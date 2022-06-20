130d54a54157565ab9c70358f66c8e7bb05eb1c9
ZDF\Administrator <2252480056@qq.com>
2022-06-20 18:11:28 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入全局样式
 import './styles/index.less'
+// 导入Vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
+
+// 注册Vant组件库
+Vue.use(Vant)
 
 new Vue({
   router,
